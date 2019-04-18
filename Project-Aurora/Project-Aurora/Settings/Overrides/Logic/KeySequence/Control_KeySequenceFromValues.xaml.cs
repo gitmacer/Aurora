bf7daac40b01fb3bb233499c305bb19edcd58e9c
Will Bennion <will.bennion@outlook.com>
2019-04-19 00:12:39 +0100

    Added KeySequence from values evaluatable.

---
+++ Project-Aurora/Project-Aurora/Settings/Overrides/Logic/KeySequence/Control_KeySequenceFromValues.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Aurora.Profiles;
+using Aurora.Settings.Overrides.Logic;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Aurora.Settings.Overrides.Logic {
+    /// <summary>
+    /// Interaction logic for Control_ColorFromValues.xaml
+    /// </summary>
+    public partial class Control_KeySequenceFromValues : UserControl {
+        public Control_KeySequenceFromValues(Application app, KeySequenceFromValues parent) {
+            InitializeComponent();
+            DataContext = new Control_KeySequenceFromValues_Context { Application = app, ParentCondition = parent };
+        }
+
+        public void SetApplication(Application app) {
+            var dc = ((Control_KeySequenceFromValues_Context)DataContext);
+            dc.Application = app;
+            dc.NotifyChanged("Application");
+        }
+
+        protected class Control_KeySequenceFromValues_Context : INotifyPropertyChanged {
+            public event PropertyChangedEventHandler PropertyChanged;
+            public void NotifyChanged(string prop) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            public Application Application { get; set; }
+            public KeySequenceFromValues ParentCondition { get; set; }
+        }
+    }
+}
--- Project-Aurora/Project-Aurora/Settings/Overrides/Logic/KeySequence/KeySequence_Ctors.cs
+++ Project-Aurora/Project-Aurora/Settings/Overrides/Logic/KeySequence/KeySequence_Ctors.cs
@@ -40,4 +40,60 @@ namespace Aurora.Settings.Overrides.Logic {
         public IEvaluatable<KeySequence> Clone() => new KeySequenceConstant((KeySequence)Sequence.Clone());
         IEvaluatable IEvaluatable.Clone() => Clone();
     }
+
+
+    /// <summary>
+    /// A KeySequence that is set from given property values.
+    /// </summary>
+    [Evaluatable("Key Sequence (Freeform from values)", category: OverrideLogicCategory.Logic)]
+    public class KeySequenceFromValues : IEvaluatable<KeySequence> {
+
+        // Evaluatables used to build the freeform object
+        public IEvaluatable<double> X { get; set; }
+        public IEvaluatable<double> Y { get; set; }
+        public IEvaluatable<double> Width { get; set; }
+        public IEvaluatable<double> Height { get; set; }
+        public IEvaluatable<double> Angle { get; set; }
+
+        /// <summary>Creates a new KeySequence constant with the default sized and positioned freeform.</summary>
+        public KeySequenceFromValues() : this(new NumberConstant(0), new NumberConstant(0)) { }
+
+        /// <summary>Creates a new KeySequence with the given evaluatables.</summary>
+        public KeySequenceFromValues(IEvaluatable<double> x, IEvaluatable<double> y, IEvaluatable<double> width = null, IEvaluatable<double> height = null, IEvaluatable<double> angle = null) {
+            X = x ?? new NumberConstant(0);
+            Y = y ?? new NumberConstant(0);
+            Width = width ?? new NumberConstant(30);
+            Height = height ?? new NumberConstant(30);
+            Angle = angle ?? new NumberConstant(0);
+        }
+
+        /// <summary>Creates a new KeySequence with the given constant values.</summary>
+        public KeySequenceFromValues(double x, double y, double width = 30, double height = 30, double angle = 0) {
+            X = new NumberConstant(x);
+            Y = new NumberConstant(y);
+            Width = new NumberConstant(width);
+            Height = new NumberConstant(height);
+            Angle = new NumberConstant(angle);
+        }
+
+        [JsonIgnore]
+        private Control_KeySequenceFromValues control;
+        public System.Windows.Media.Visual GetControl(Application application) => control ?? (control = new Control_KeySequenceFromValues(application, this));
+
+        /// <summary>Evaluate conditions and return the appropriate evaluation.</summary>
+        public KeySequence Evaluate(IGameState gameState) => new KeySequence(new FreeFormObject(
+            (float)X.Evaluate(gameState), (float)Y.Evaluate(gameState), // Position
+            (float)Width.Evaluate(gameState), (float)Height.Evaluate(gameState), // Size
+            (float)Angle.Evaluate(gameState) // Angle
+        ));
+
+        object IEvaluatable.Evaluate(IGameState gameState) => Evaluate(gameState);
+
+        /// <summary>Application-independent </summary>
+        public void SetApplication(Application application) { }
+
+        /// <summary>Clones this KeySequenceConstant.</summary>
+        public IEvaluatable<KeySequence> Clone() => new KeySequenceFromValues();
+        IEvaluatable IEvaluatable.Clone() => Clone();
+    }
 }
